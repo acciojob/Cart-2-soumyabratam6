776290e37f7b33ea06f419c61c78ead4e98ed96f
soumyabrata <soumyabratam6@gmail.com>
2024-09-17 21:38:26 +0530

    all file added and fix

--- src/cartReducer.js
+++ src/cartReducer.js
@@ -1,7 +1,7 @@
 const cartReducer = (state, action)=>{
 switch(action.type){
     case'ADD_TO_CART':
-    return[...state, { ...action.product, quantity: 1 }];
+    return[...state, { ...action.product, quantity: 3 }];
 
     case 'INCREASE_QUANTITY':
         return state.map(item=>item.id === action.id?{...item,quantity:item.quantity+1}:item)
